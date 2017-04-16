aa33684c59234ea8081894b1225989337b3c4920
purva5194 <purvapatel@Purvas-MacBook-Pro.local>
2017-04-15 21:06:43 -0700

    log page added

--- SampleApplication/app/src/main/java/com/example/purvapatel/sampleapplication/Log.java
+++ SampleApplication/app/src/main/java/com/example/purvapatel/sampleapplication/Log.java
@@ -6,16 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by purvapatel on 4/12/17.
  */
 
 public class Log extends Fragment {
+    EditText logs;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return getActivity().getLayoutInflater().inflate(R.layout.activity_log, container , false);
+        View view = getActivity().getLayoutInflater().inflate(R.layout.activity_log, container , false);
+
+        logs = (EditText) view.findViewById(R.id.logs);
+
+        logs.setText("Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n" +
+                "Date : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\nDate : 4/12/2017 Light : off\n");
+        return view;
     }
 
     @Override
